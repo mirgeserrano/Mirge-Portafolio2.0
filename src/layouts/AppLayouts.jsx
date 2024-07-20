8296885b28c82cae03cc22f28e174d05c3eea335
mirge <you@example.com>
2024-07-20 19:13:59 -0400

    20/7/2024 0.3

--- src/layouts/AppLayouts.jsx
+++ src/layouts/AppLayouts.jsx
@@ -4,11 +4,15 @@ import { NavbarBox } from "../components/NavbarBox"
 const AppLayouts = () => {
   return (
     <div
-    className="bg-fondo dark:bg-fondoDark min-h-screen bg-no-repeat bg-center bg-cover bg-fixed md:pb-16 w-full "
+    className="min-h-screen bg-no-repeat bg-center bg-cover bg-fixed md:pb-16 w-full "
     data-aos="fade"
+    style={{
+          backgroundImage: url(${darkMode ? fondoDark : fondo}),
+        }}
   >
     <Navbar />
     <div className="container mx-auto grid grid-cols-12 md:gap-10  justify-between lg:mt-[220px]  ">
+    
       <InfBox />
       <NavbarBox />
     </div>
